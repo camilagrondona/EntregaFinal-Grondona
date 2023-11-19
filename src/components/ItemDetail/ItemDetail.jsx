2a7d299dbea9da38495633936078afbfa2075675
Camila Grondona <grondonacamila@gmail.com>
2023-11-19 18:08:05 +0100

    ItemDetail

---
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,22 @@
+import { ItemCount } from "../ItemCount/ItemCount";
+
+export const ItemDetail = ({ description, image, price, stock, name }) => {
+
+    const onAdd = (items) => {
+        alert(`Se agregaron ${items} al carrito`)
+    }
+
+    return (
+        <div className="border m-2">
+            <div className="card ">
+                <div className="card-body ">
+                    <h5 className="card-title">{name}</h5>
+                    <img src={image} alt="" />
+                    <p className="card-text"> {description} </p>
+                    <p>Precio: {price} </p>
+                    <ItemCount stock={stock} onAdd={onAdd} />
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react"
+import { getProductById } from "../../asyncMock"
+import { ItemDetail } from "../ItemDetail/ItemDetail"
+import { useParams } from "react-router-dom"
+
 export const ItemDetailContainer = () => {
+    const { id } = useParams()
+    const [item, setItem] = useState(null)
+
+    useEffect(() => {
+        getProductById(id)
+            .then(resp => setItem(resp))
+            .catch(error => console.log(error))
+    }, [item])
+
     return (
-        <div>ItemDetailContainer</div>
+
+        <>
+            {item && <ItemDetail {...item} />}
+        </>
     )
 }
