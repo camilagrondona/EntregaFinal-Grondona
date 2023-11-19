9d6b051f9cb7fb421e3e063856248a1d5989384c
Camila Grondona <grondonacamila@gmail.com>
2023-11-19 18:27:28 +0100

    isLoading ItemDetailContainer

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -6,17 +6,27 @@ import { useParams } from "react-router-dom"
 export const ItemDetailContainer = () => {
     const { id } = useParams()
     const [item, setItem] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
+        setIsLoading(true)
+
         getProductById(id)
-            .then(resp => setItem(resp))
-            .catch(error => console.log(error))
-    }, [item])
+            .then((resp) => {
+                setItem(resp)
+                setIsLoading(false)
+            })
+            .catch((error) => {
+                console.log(error)
+                setIsLoading(false)
+            })
+
+    }, [id])
 
     return (
 
         <>
-            {item && <ItemDetail {...item} />}
+            {isLoading ? <h2> Cargando detalles del producto...</h2> : item && <ItemDetail {...item} />}
         </>
     )
 }
