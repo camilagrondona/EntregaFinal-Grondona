9f91ca44d29a2b280c5e5aff86179b801cf72bdf
Camila Grondona <grondonacamila@gmail.com>
2023-12-16 17:36:08 +0100

    CartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,16 +1,13 @@
 import { BsCart3 } from "react-icons/bs"
 import { useCart } from "../../context/CartContext"
-import { Link } from 'react-router-dom'
 
 export const CartWidget = () => {
     const { totalQuantity } = useCart()
 
     return (
-        <Link to="/cart">
             <div className="d-flex">
                 <BsCart3 color="#58ACFA" size={30} />
                 <span>({totalQuantity})</span>
             </div>
-        </Link>
     )
 }
