d6431a29089c8325271fae22e51f73016ddea723
Camila Grondona <grondonacamila@gmail.com>
2023-12-16 17:58:30 +0100

    botones carrito

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -12,7 +12,7 @@ export const Cart = () => {
         if (cartItems.length === 0) {
             Swal.fire({
                 title: "Carrito de compras vacio",
-                text: "Por favor, revisa que hayas agregado todos los items",
+                text: "Por favor, agrega productos antes de finalizar la compra",
                 icon: "error"
             })
 
@@ -32,8 +32,8 @@ export const Cart = () => {
                         <p>Cantidad: {item.quantity}</p>
                         <p>Subtotal: ${item.subTotal}</p>
                         <div>
-                            <button onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>Incrementar</button>
-                            <button onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>Reducir</button>
+                            <button onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>Agregar</button>
+                            <button onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>Quitar</button>
                         </div>
                         <button onClick={() => removeItem(item.id)}>
                             Eliminar
